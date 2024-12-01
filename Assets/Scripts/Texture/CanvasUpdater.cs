606ef1232644d277551a30a824b61af46a738349
Timon Sommer <timon@timonso.com>
2024-12-01 16:35:53 +0000

    remove build.apk and update script

--- Assets/Scripts/Texture/CanvasUpdater.cs
+++ Assets/Scripts/Texture/CanvasUpdater.cs
@@ -8,7 +8,7 @@ public class CanvasUpdater : MonoBehaviour
     public Material[] materials;
     public Texture2D current_canvas_texture;
 
-    public string texture_url = "https://d6be0271-d571-42d8-a09b-305d7f148d1f.client-api.unity3dusercontent.com/client_api/v1/environments/a692e5af-8842-4289-bb91-ad105fd3e95d/buckets/29e6096a-5d6c-4985-a63c-ed8a16000af9/entries/3a3cd862-6ada-4fe9-b7ae-dac3d386abc1/versions/f83c7f17-f247-4d82-89a0-d4df005bd1ef/content/";
+    public string texture_url = "https://d6be0271-d571-42d8-a09b-305d7f148d1f.client-api.unity3dusercontent.com/client_api/v1/environments/production/buckets/29e6096a-5d6c-4985-a63c-ed8a16000af9/release_by_badge/latest/entry_by_path/content/?path=image.jpg";
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,6 +22,7 @@ public class CanvasUpdater : MonoBehaviour
         if (is_Changed)
         {
             StartCoroutine(fetchTexture(texture_url));
+            is_Changed = false;
         }
     }
 
@@ -40,7 +41,7 @@ public class CanvasUpdater : MonoBehaviour
             {
 
             }
-            is_Changed = false;
+            
         }
     }
 }
