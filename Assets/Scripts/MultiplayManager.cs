3dc1b6ce5fecbc66716bf1e223660d303f330822
Timon Sommer <timon@timonso.com>
2024-12-02 16:37:07 +0000

    new network manager

---
+++ Assets/Scripts/MultiplayManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Unity.Services.Core;
+using Unity.Services.Multiplay;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using System.Threading.Tasks;
+using System.Net;
+
+public class MultiplayManager : MonoBehaviour
+{
+    [SerializeField] private string ipAddress;
+    [SerializeField] private ushort port;
+
+    private IServerQueryHandler serverQueryHandler;
+
+    private async void Start()
+    {
+        if (Application.platform == RuntimePlatform.LinuxServer)
+        {
+            Application.targetFrameRate = 60;
+        }
+
+        await UnityServices.InitializeAsync();
+
+        ServerConfig serverConfig = MultiplayService.Instance.ServerConfig;
+        serverQueryHandler = await MultiplayService.Instance.StartServerQueryHandlerAsync(10, "MyServer", "MyGameType", "0", "TestMap");
+
+        if (serverConfig.AllocationId != string.Empty)
+        {
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData("0.0.0.0", serverConfig.Port, "0.0.0.0");
+            NetworkManager.Singleton.StartServer();
+
+            await MultiplayService.Instance.ReadyServerForPlayersAsync();
+        }
+    }
+
+    private async void Update()
+    {
+        if (Application.platform == RuntimePlatform.LinuxServer)
+        {
+            if (serverQueryHandler != null)
+            {
+                serverQueryHandler.CurrentPlayers = (ushort)NetworkManager.Singleton.ConnectedClientsIds.Count;
+                serverQueryHandler.UpdateServerCheck();
+                await Task.Delay(100);
+            }
+        }
+    }
+
+    public void JoinToServer()
+    {
+        UnityTransport transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+        transport.SetConnectionData(ipAddress, port);
+
+        NetworkManager.Singleton.StartClient();
+    }
+
+}
