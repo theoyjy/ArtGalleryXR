f90b7cd28dd8551ed086b28561c4bd75c3c60b42
D-Stobie <43788011+D-Stobie@users.noreply.github.com>
2025-03-20 16:32:38 +0000

    merge fix

--- Assets/Whiteboard/Scripts/MarkerColorChanger.cs
+++ Assets/Whiteboard/Scripts/MarkerColorChanger.cs
@@ -1,12 +1,11 @@
 using UnityEngine;
-using System.IO;
 
 public class MarkerColorChanger : MonoBehaviour
 {
-    //public Color newColor = Color.red; // Default color
+    public Color newColor = Color.red; // Default color
     public GameObject tip;
     private Material markerMaterial;
-   
+
     void Start()
     {
         // Find the material named "Marker" in the Resources folder or assigned materials
@@ -21,30 +20,14 @@ public class MarkerColorChanger : MonoBehaviour
 
     void Update()
     {
-        //markerMaterial.color = newColor;
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            ChangeColor(Color.green);
-        }
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            ChangeColor(Color.red);
-        }
-        if (Input.GetKeyDown(KeyCode.B))
-        {
-            ChangeColor(Color.blue);
-        }
-        if (Input.GetKeyDown(KeyCode.Y))
-        {
-            ChangeColor(Color.yellow);
-        }
+        markerMaterial.color = newColor;
     }
 
     public void ChangeColor(Color color)
     {
         if (markerMaterial != null)
         {
-            markerMaterial.color = color;
+            markerMaterial.color = newColor;
         }
     }
 }
